d90dd78919b05e80f3e37e370bf710dda2a9ca74
Joshua Aguirre <30274013+JoshSDchargers@users.noreply.github.com>
2022-04-12 19:05:30 -0700

    Update FoldersEmpty.ps1

--- FoldersEmpty.ps1
+++ FoldersEmpty.ps1
@@ -1,5 +1,5 @@
 ﻿Start-Transcript -Path .\Desktop\EmptyFolder.txt
 
-Get-ChildItem -Path \\picr.local\Files\users -Directory | Where-Object {$_.GetFiles().Count -eq 0}
+Get-ChildItem -Path \\domain.local\Files\users -Directory | Where-Object {$_.GetFiles().Count -eq 0}
 
 Stop-Transcript
